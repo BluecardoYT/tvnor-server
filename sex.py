2a655f9a35dca134fbd0950b40b4949f859948bc
BluecardoYT <ricardothesprunki@gmail.com>
2024-12-30 23:29:40 -0300

    Create sex.py

---
+++ sex.py
@@ -0,0 +1,16 @@
+url = f"https://tenor.com/view/{name[6:]}
+img_url = https://raw.githubusercontent.com/BluecardoYT/tvnor-server/refs/heads/main/view.png
+img_raw = requests.get(img.url).raw
+
+img = Image.open(img_raw).resize((350, 185))
+draw = ImageDraw.Draw(img)
+font = truetype("impact.ttf", 32)
+
+draw.text = ((350 // 2, 0), "WTF DISCORD SEX SEX")
+draw.text = ((350 // 2, 185), "???WTF??? ???SEX???")
+
+jpg = io.BytesIO()
+img = Image.open(jpg).resize((190, 135))
+base = Image.open(io.BytesIO(base_img))
+base.paste(io.BytesIO(base_img))
+base.paste(img, (5, 159))
